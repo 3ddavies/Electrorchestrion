980117300bb60f54a7dd2c001922b13e7d8e3dd6
Gabriel Peressini <gabe.peressini@gmail.com>
2020-01-06 22:56:05 -0500

    worked on event parsing

--- MidiParse/Tests/mididictionaries.py
+++ MidiParse/Tests/mididictionaries.py
@@ -15,3 +15,34 @@ midimeta = {
 "59":["Key Signature", 1, 2, False],
 "7f":["Sequencer-Specific Meta-Event", 0, 'vlv', False]
 }
+"""
+Command	Meaning				# parameters	param 1			param 2
+0x80	Note-off				2			key				velocity
+0x90	Note-on					2			key				veolcity
+0xA0	Aftertouch				2			key				touch
+0xB0	Continuous controller	2			controller #	controller value
+0xC0	Patch Change			2			instrument #	
+0xD0	Channel Pressure		1			pressure
+0xE0	Pitch bend				2			lsb (7 bits)	msb (7 bits)
+"""
+#print(musicevents[bytestring[0]][0] + " on channel "+str(bytestring[1]))
+musicalevents = {
+"8":["Note Off", 2],
+"9":["Note On", 2],
+"a":["Aftertouch", 2],
+"b":["Continuous Controller", 2],
+"c":["Patch Change", 1],
+"d":["Channel Pressure", 1],
+"e":["Pitch Bend", 2]
+}
+#ff59 0203 00
+keysignaturesf = {
+""
+}
+twoscomplement = {
+	
+}
+"""
+,
+"f":["(non-musical commands)", ]
+"""
